b7e18341b64255328407fc6c8e7388fae4ec72e2
陈雪丹 <chenxuedan@toukeiweideimac.local>
2016-07-19 09:52:17 +0800

    Initial Commit

---
+++ MixApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MixApp
+//
+//  Created by 陈雪丹 on 16/7/19.
+//  Copyright © 2016年 cxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MixAppTests/MixAppTests.m
@@ -0,0 +1,39 @@
+//
+//  MixAppTests.m
+//  MixAppTests
+//
+//  Created by 陈雪丹 on 16/7/19.
+//  Copyright © 2016年 cxd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MixAppTests : XCTestCase
+
+@end
+
+@implementation MixAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
